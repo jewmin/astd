1fa6a56a602b69d109027b62854de10e9d60d447
jim chen <jewmin@gmail.com>
2017-06-18 09:31:42 +0800

    增加XML解析通用组件

---
+++ com.lover.astd.common/XmlHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using com.lover.astd.common.model;
+
+namespace com.lover.astd.common
+{
+    public class XmlHelper
+    {
+        /// <summary>
+        /// 获取数值类型的值, 如int, float, double
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="node"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T GetValue<T>(XmlNode node, T def = default(T)) where T : struct
+        {
+            T t = def;
+            if (node != null) t = (T)Convert.ChangeType(node.InnerText, typeof(T));
+            return t;
+        }
+        /// <summary>
+        /// 获取字符串类型的值
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static string GetString(XmlNode node, string def = "")
+        {
+            string t = def;
+            if (node != null) t = node.InnerText;
+            return t;
+        }
+        /// <summary>
+        /// 返回类类型的值, 类必须继承XmlObject类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="node"></param>
+        /// <param name="def"></param>
+        /// <returns></returns>
+        public static T GetClass<T>(XmlNode node, T def = default(T)) where T : new()
+        {
+            T t = def;
+            if (node != null) { t = new T(); (t as XmlObject).Parse(node); }
+            return t;
+        }
+        /// <summary>
+        /// 返回类类型列表，类必须继承XmlObject类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public static List<T> GetClassList<T>(XmlNodeList nodelist) where T : new()
+        {
+            List<T> list = new List<T>();
+            if (nodelist != null)
+            {
+                foreach (XmlNode node in nodelist)
+                {
+                    T t = new T();
+                    (t as XmlObject).Parse(node);
+                    if ((t as XmlObject).CanAdd()) list.Add(t);
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ com.lover.astd.common/model/XmlObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace com.lover.astd.common.model
+{
+    public abstract class XmlObject
+    {
+        /// <summary>
+        /// 解析xml
+        /// </summary>
+        /// <param name="node"></param>
+        public abstract void Parse(XmlNode node);
+        /// <summary>
+        /// 是否能插入到列表
+        /// </summary>
+        /// <returns></returns>
+        public abstract bool CanAdd();
+    }
+}
