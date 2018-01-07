9322be99a2382d7879c5d54ad431bea0efa2bd44
jim chen <jewmin@gmail.com>
2018-01-07 10:13:05 +0800

    修改大将训练顺序 增加每日对战任务完成 修改项目配置，增加自动拷贝

--- com.lover.astd.common/model/User.cs
+++ com.lover.astd.common/model/User.cs
@@ -159,6 +159,10 @@ namespace com.lover.astd.common.model
         /// 每日任务炼制次数
         /// </summary>
         public int _refine_task_num = 0;
+        /// <summary>
+        /// 每日任务对战次数
+        /// </summary>
+        public int _kfrank_task_num = 0;
 
 		public int[] _fete_min_levels;
 
